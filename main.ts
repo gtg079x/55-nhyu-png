8e9b7445dd743cb40c03e81827fe555d975a95c6
gtg079x <67651413+gtg079x@users.noreply.github.com>
2020-12-19 16:55:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onButtonPressed(Button.A, function () {
     radio.sendString("hi mom")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("i'm hungry can you make dinner")
 })
 radio.setGroup(10)
